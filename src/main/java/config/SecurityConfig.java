135ff5935c208eaf3bc1c92fd031ce1ae8908bf1
biraudaniela <58104755+biraudaniela@users.noreply.github.com>
2020-09-05 15:23:06 +0300

    add security

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,48 +18,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Configuration
-//    @Order(1)
-//    @EnableGlobalMethodSecurity(prePostEnabled = true)
-//    public static class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//        @Autowired
-//        private DatabaseUserDetailsService databaseUserDetailsService;
-//
-//        @Autowired
-//        protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//            auth.userDetailsService(databaseUserDetailsService).passwordEncoder(restPasswordEncoder());
-//        }
+    @Configuration
+    @Order(1)
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Autowired
+        private DatabaseUserDetailsService databaseUserDetailsService;
 
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http
-//                    .antMatcher("/api/**")
-//                    .cors() //cross origin request site
-//                    .and()
-//                    .csrf()
-//                    .disable()
-//                    .authorizeRequests()
-//                    //do not allow anything else
-//                    .antMatchers("/api/login").permitAll()
-//                    .anyRequest().authenticated();
-//
-//            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//            http.addFilterBefore(new UsernameAndPasswordAuthenticationFilter(databaseUserDetailsService),
-//                    UsernameAndPasswordAuthenticationFilter.class);
-//
-//
-//        }
-//
-//        @Bean
-//        public PasswordEncoder restPasswordEncoder() {
-//            return new BCryptPasswordEncoder();
-//        }
-//
-//    }
+        @Autowired
+        protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(databaseUserDetailsService).passwordEncoder(restPasswordEncoder());
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/api/**")
+                    .cors() //cross origin request site
+                    .and()
+                    .csrf()
+                    .disable()
+                    .authorizeRequests()
+                    //do not allow anything else
+                    .antMatchers("/api/login").permitAll()
+                    .anyRequest().authenticated();
+
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+
+        @Bean
+        public PasswordEncoder restPasswordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+    }
 
     @Configuration
- //   @Order(2)
+   @Order(2)
     public static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Autowired
         private DatabaseUserDetailsService databaseUserDetailsService;
