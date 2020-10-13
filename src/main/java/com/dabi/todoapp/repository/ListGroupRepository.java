666c73b6cb165b847624eb89011ae3ed68db5f98
biraudaniela <58104755+biraudaniela@users.noreply.github.com>
2020-10-13 11:12:17 +0300

    lists

--- src/main/java/com/dabi/todoapp/repository/ListGroupRepository.java
+++ src/main/java/com/dabi/todoapp/repository/ListGroupRepository.java
@@ -2,9 +2,12 @@ package com.dabi.todoapp.repository;
 
 import com.dabi.todoapp.model.ListGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ListGroupRepository extends JpaRepository<ListGroup, Integer> {
-    List<ListGroup> findByUserId(Integer id);
+    @Query("FROM ListGroup l WHERE l.user.userId=:userId")
+    List<ListGroup> findByUserId(@Param("userId")Integer id);
 }
